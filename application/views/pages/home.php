21004d3f62383e0e6a66cbe735823417045e7994
Lohit <lohit.pereira09@stanislausbandra.in>
2017-04-10 14:00:45 +0530

    Landnig homepage Text/fonts done

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,10 +2,11 @@
 <div class="container">
 	<div style="margin: 0; width:100%; height: 50%;background-position: center;background-size: cover;">
 		<div class="container">
-			<div class="page-header">
+			<!--div class="page-header">
 				<h1 class="display-3">Lohit Pereira</h1>
-			</div>
-			<p class="lead">Professional Web develpment.</p>
+			</div-->
+			<h1 align="center" class="gFont-lobster" style="font-size: 205px;" class="cover-heading">Lohit Pereira</h1>
+			<center><p class="lead"><h2 class="gFont-montserrat">Web site Development | Design | SEO</h2></p></center>
 		</div>
 	</div>
 </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
         <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
         <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-theme.css">
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
         
         
     </head>
