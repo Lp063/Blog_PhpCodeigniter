4ad16ec0ba0dc5ed524b719b0a17e71c8fbbcda0
Lohit <lohit.pereira09@stanislausbandra.in>
2017-04-11 13:56:12 +0530

    updated style for nave link

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,8 +7,7 @@
         <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
         <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-theme.css">
         <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-        
-        
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
     </head>
     <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script-->
     <script src="http://cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
@@ -16,7 +15,7 @@
     <script src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
     <script src="<?php echo base_url();?>assets/js/jquery-3.1.1.min.js"></script>
     <body <?php if(isset($bgimg)){echo "background='".$bgimg."' ";}?> style="margin: 0; width:100%; height: 100%; background-position: center;background-repeat: no-repeat;background-size: cover;">
-        <nav class="navbar navbar-default">
+        <nav>
             <div class="container-fluid">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="<?php echo base_url(); ?>">
@@ -25,11 +24,11 @@
                 </div>
                 <div id="navbar">
                     <ul class="nav navbar-nav">
-                        <li><a href="<?php echo base_url(); ?>">Home</a></li>
-                        <li><a href="<?php echo base_url(); ?>about">About</a></li>
-                        <li><a href="<?php echo base_url(); ?>posts/index">Blog</a></li>
+                        <li><a href="<?php echo base_url(); ?>" class="navlink">Home</a></li>
+                        <li><a href="<?php echo base_url(); ?>about" class="navlink">About</a></li>
+                        <li><a href="<?php echo base_url(); ?>posts/index" class="navlink">Blog</a></li>
                         <ul class="nav navbar-nav navbar-right">
-                            <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
+                            <li><a href="<?php echo base_url(); ?>posts/create" class="navlink">Create Post</a></li>
                         </ul>
                     </ul>
                 </div>
