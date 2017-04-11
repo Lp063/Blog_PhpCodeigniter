e92cdaf35e445d36c543854d8ac07abf37f5b61b
Lohit <lohit.pereira09@stanislausbandra.in>
2017-04-11 18:29:13 +0530

    changed tagline

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -6,7 +6,7 @@
 				<h1 class="display-3">Lohit Pereira</h1>
 			</div-->
 			<h1 align="center" class="gFont-lobster" style="font-size: 205px;" class="cover-heading">Lohit Pereira</h1>
-			<center><p class="lead"><h2 class="gFont-montserrat">Web site Development | Design | SEO</h2></p></center>
+			<center><p class="lead"><h2 class="gFont-montserrat">Web Development & Design</h2></p></center>
 		</div>
 	</div>
 </div>
